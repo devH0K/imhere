c9a0869781e8fe0abb3a52c8c64b18127023e1b8
devH0K <devhok@gmail.com>
2023-12-10 21:39:28 -0300

    utilizando stylesheet

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 // importacao de elementos
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 // funcao padrao de criacao
 // ao ser inicializada a aplicao, esta funcao é a primeira a ser chamada
@@ -9,23 +9,29 @@ export default function App(){
   // os componentes em JSX sempre iniciam com primeira letra maiuscula e extensao precisa ser formato .tsx
   return(
     // obs: um componente não pode retornar mais de um elemento
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Nome do Evento</Text>
-      <Text style={{
-        color: '#6B6B6B',
-        fontSize: 16,
-      }}>
+      <Text style={styles.eventDate}>
         Domingo, 10 de Dezembro de 2023</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+});
