f4d8e5105f9c7ccb5ca44a6f8b9a84168284b79a
devH0K <devhok@gmail.com>
2023-12-10 21:12:45 -0300

    entendendo o JSX

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// importacao de elementos
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+// funcao padrao de criacao
+// ao ser inicializada a aplicao, esta funcao é a primeira a ser chamada
+export default function App(){
+  // componente / interface são iguais
+  // no return, sao inseridos os elementos que vao ser exibidos em tela
+  // os componentes em JSX sempre iniciam com primeira letra maiuscula e extensao precisa ser formato .tsx
+  return(
+    // obs: um componente não pode retornar mais de um elemento
+    <View>
+      <Text>Leonardo</Text>
+      <Text>Pegoretti</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
