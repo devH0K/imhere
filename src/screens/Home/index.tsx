3723f1b9c319c4026ff3b36148ac3c90db2edd0a
devH0K <devhok@gmail.com>
2023-12-10 22:13:55 -0300

    botao de adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 // importacao de elementos
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles'
 
 // funcao padrao de criacao
@@ -8,18 +8,30 @@ export default function Home(){
   // componente / interface são iguais
   // no return, sao inseridos os elementos que vao ser exibidos em tela
   // os componentes em JSX sempre iniciam com primeira letra maiuscula e extensao precisa ser formato .tsx
+  
+  function handleParticipantAdd(){
+    console.log("Você clicou no botão adicionar!")
+  }
+  
   return(
     // obs: um componente não pode retornar mais de um elemento
     <View style={styles.container}>
+
       <Text style={styles.eventName}>
         Nome do Evento</Text>
+
       <Text style={styles.eventDate}>
         Domingo, 10 de Dezembro de 2023</Text>
+
     <TextInput
         style={styles.input}
         placeholder='Nome do participante'
         placeholderTextColor="#6B6B6B"
     />
+
+    <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
     </View>
   )
 }
